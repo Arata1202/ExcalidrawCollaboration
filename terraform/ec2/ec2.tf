8096a4110a191b0b166e2c9e0a603cf8c0f1722a
Arata <aratadayoon@yahoo.co.jp>
2026-07-02 00:32:47 +0900

    feat: add initial Terraform configuration for Excalidraw infrastructure

---
+++ terraform/ec2/ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "excalidraw_server" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  iam_instance_profile   = aws_iam_instance_profile.excalidraw_iam_instance_profile.name
+  vpc_security_group_ids = [aws_security_group.excalidraw_sg.id]
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  root_block_device {
+    volume_type           = var.volume_type
+    volume_size           = var.volume_size
+    encrypted             = var.volume_encrypted
+    kms_key_id            = var.volume_kms_key_id
+    delete_on_termination = false
+  }
+
+  tags = {
+    Name = "excalidraw_server"
+  }
+}
